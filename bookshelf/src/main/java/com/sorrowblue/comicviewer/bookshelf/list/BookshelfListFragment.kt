3f47bbd0e0fc05015a37091aff77c9a547caf156
SorrowBlue <sorrowblue.dev@gmail.com>
2023-06-25 21:15:30 +0900

    :construction: 本棚情報画面をComposeに置き換える。(#147)

--- bookshelf/src/main/java/com/sorrowblue/comicviewer/bookshelf/list/BookshelfListFragment.kt
+++ bookshelf/src/main/java/com/sorrowblue/comicviewer/bookshelf/list/BookshelfListFragment.kt
@@ -11,11 +11,7 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.accompanist.themeadapter.material3.Mdc3Theme
 import com.google.android.material.transition.MaterialFadeThrough
-import com.sorrowblue.comicviewer.bookshelf.R
-import com.sorrowblue.comicviewer.bookshelf.info.BookshelfInfoEditResult
-import com.sorrowblue.comicviewer.bookshelf.info.BookshelfInfoRemoveResult
 import com.sorrowblue.comicviewer.framework.ui.fragment.FrameworkFragment
-import com.sorrowblue.comicviewer.framework.ui.navigation.setDialogFragmentResultListener
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -36,35 +32,6 @@ internal class BookshelfListFragment : FrameworkFragment() {
     ): View {
         fab.setOnClickListener {
             findNavController().navigate(BookshelfListFragmentDirections.actionBookshelfListToBookshelfManageList())
-        }
-        setDialogFragmentResultListener<BookshelfInfoRemoveResult>(
-            R.id.bookshelf_list_fragment,
-            "remove"
-        ) {
-            findNavController().navigate(
-                BookshelfListFragmentDirections.actionBookshelfListToBookshelfRemoveConfirm(it.bookshelfId.value)
-            )
-        }
-
-        setDialogFragmentResultListener<BookshelfInfoEditResult>(
-            R.id.bookshelf_list_fragment,
-            "edit"
-        ) {
-            when (it.type) {
-                "InternalStorage" -> {
-                    findNavController().navigate(
-                        BookshelfListFragmentDirections.actionBookshelfListToBookshelfManageDevice(
-                            it.bookshelfId.value
-                        )
-                    )
-                }
-
-                "SmbServer" -> {
-                    findNavController().navigate(
-                        BookshelfListFragmentDirections.actionBookshelfListToBookshelfManageSmb(it.bookshelfId.value)
-                    )
-                }
-            }
         }
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
--- bookshelf/src/main/java/com/sorrowblue/comicviewer/bookshelf/list/BookshelfListScreen.kt
+++ bookshelf/src/main/java/com/sorrowblue/comicviewer/bookshelf/list/BookshelfListScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
@@ -19,19 +20,26 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.Typography
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -47,7 +55,11 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemContentType
 import androidx.paging.compose.itemKey
+import com.sorrowblue.comicviewer.bookshelf.BookshelfConverter.source
 import com.sorrowblue.comicviewer.bookshelf.R
+import com.sorrowblue.comicviewer.domain.entity.BookshelfFolder
+import com.sorrowblue.comicviewer.domain.entity.bookshelf.InternalStorage
+import com.sorrowblue.comicviewer.domain.entity.bookshelf.SmbServer
 import com.sorrowblue.comicviewer.domain.entity.file.Folder
 import com.sorrowblue.comicviewer.folder.FolderFragmentArgs
 
@@ -87,6 +99,8 @@ internal fun BookshelfListScreen(
         val nestedScrollConnection = rememberNestedScrollInteropConnection()
         val isRefreshing = viewModel.isRefreshingFlow.collectAsState()
         val isEmptyData = viewModel.isEmptyDataFlow.collectAsState()
+        var bookshelfFolder by remember { mutableStateOf<BookshelfFolder?>(null) }
+        var showBottomSheet by remember { mutableStateOf(false) }
         if (isEmptyData.value) {
             Column(
                 modifier = Modifier.fillMaxSize(),
@@ -100,7 +114,7 @@ internal fun BookshelfListScreen(
                 )
                 Text(
                     text = stringResource(id = R.string.bookshelf_list_message_no_bookshelves_added_yet),
-                    style = Typography().headlineSmall
+                    style = MaterialTheme.typography.headlineSmall
                 )
             }
         } else {
@@ -133,11 +147,8 @@ internal fun BookshelfListScreen(
                                     .fillMaxWidth()
                                     .combinedClickable(
                                         onLongClick = {
-                                            navController.navigate(
-                                                BookshelfListFragmentDirections.actionBookshelfListToBookshelfInfo(
-                                                    item.bookshelf.id.value
-                                                )
-                                            )
+                                            bookshelfFolder = item
+                                            showBottomSheet = true
                                         },
                                         onClick = {
                                             navController.navigate(
@@ -148,6 +159,98 @@ internal fun BookshelfListScreen(
                                         }
                                     )
                             )
+                        }
+                    }
+                }
+            }
+            val sheetState = rememberModalBottomSheetState(false)
+            if (showBottomSheet && bookshelfFolder != null) {
+                val bookshelf = bookshelfFolder!!.bookshelf
+                val folder = bookshelfFolder!!.folder
+                ModalBottomSheet(
+                    onDismissRequest = { showBottomSheet = false },
+                    sheetState = sheetState
+                ) {
+                    Column(Modifier.padding(horizontal = 16.dp)) {
+                        Text(
+                            text = stringResource(id = bookshelf.source()),
+                            style = MaterialTheme.typography.labelSmall,
+                            modifier = Modifier.align(Alignment.End)
+                        )
+                        Text(
+                            text = bookshelf.displayName,
+                            style = MaterialTheme.typography.titleLarge
+                        )
+
+                        when (bookshelf) {
+                            is InternalStorage -> {
+                                Text(
+                                    text = stringResource(id = R.string.bookshelf_info_label_path),
+                                    style = MaterialTheme.typography.labelSmall,
+                                    modifier = Modifier.padding(top = 16.dp)
+                                )
+                                Text(
+                                    text = folder.path,
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                            }
+                            is SmbServer -> {
+                                Text(
+                                    text = stringResource(id = R.string.bookshelf_info_label_host),
+                                    style = MaterialTheme.typography.labelSmall,
+                                    modifier = Modifier.padding(top = 16.dp)
+                                )
+                                Text(
+                                    text = bookshelf.host,
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                                Text(
+                                    text = stringResource(id = R.string.bookshelf_info_label_port),
+                                    style = MaterialTheme.typography.labelSmall,
+                                    modifier = Modifier.padding(top = 16.dp)
+                                )
+                                Text(
+                                    text = bookshelf.port.toString(),
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                                Text(
+                                    text = stringResource(id = R.string.bookshelf_info_label_path),
+                                    style = MaterialTheme.typography.labelSmall,
+                                    modifier = Modifier.padding(top = 16.dp)
+                                )
+                                Text(
+                                    text = folder.path,
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                            }
+                        }
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
+                        ) {
+                            Button(onClick = {
+                                navController.navigate(
+                                    BookshelfListFragmentDirections.actionBookshelfListToBookshelfRemoveConfirm(bookshelf.id.value)
+                                )
+                            }) {
+                                Text(text = stringResource(id = R.string.bookshelf_info_btn_remove))
+                            }
+                            FilledTonalButton(onClick = {
+                                when (bookshelf) {
+                                    is InternalStorage ->
+                                        navController.navigate(
+                                            BookshelfListFragmentDirections.actionBookshelfListToBookshelfManageDevice(
+                                                bookshelf.id.value
+                                            )
+                                        )
+                                    is SmbServer ->
+                                        navController.navigate(
+                                            BookshelfListFragmentDirections.actionBookshelfListToBookshelfManageSmb(bookshelf.id.value)
+                                        )
+                                }
+                            }) {
+                                Text(text = stringResource(id = R.string.bookshelf_info_btn_edit))
+                            }
                         }
                     }
                 }
