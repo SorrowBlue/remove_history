2cab2ffb4706cb8e8bdd27c82ae2a8e89b71eac1
SorrowBlue <sorrowblue.dev@gmail.com>
2024-01-02 06:08:16 +0900

    :adhesive_bandage: Add Compose Destinations support to Android modules.(#288)

--- build-logic/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
+++ build-logic/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
@@ -1,16 +1,35 @@
 import com.android.build.api.dsl.ApplicationExtension
+import com.google.devtools.ksp.gradle.KspExtension
 import com.sorrowblue.comicviewer.configureAndroidCompose
+import com.sorrowblue.comicviewer.implementation
+import com.sorrowblue.comicviewer.ksp
+import com.sorrowblue.comicviewer.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 
 internal class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         with(target) {
-            pluginManager.apply("com.android.application")
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("com.google.devtools.ksp")
+            }
+
             val extension = extensions.getByType<ApplicationExtension>()
             configureAndroidCompose(extension)
+
+            dependencies {
+                implementation(libs.findLibrary("compose-destinations-core").get())
+                ksp(libs.findLibrary("compose-destinations-ksp").get())
+            }
+
+            configure<KspExtension> {
+                arg("compose-destinations.mode", "destinations")
+            }
         }
     }
 }
