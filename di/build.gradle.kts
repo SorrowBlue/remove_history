b971e9968dc707e9a1d0a38a8fc9619819fd37c4
SorrowBlue <sorrowblue.dev@gmail.com>
2024-01-25 21:32:55 +0900

    :adhesive_bandage: Update Google Play Services and libraries versions.(#288)

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -25,4 +25,11 @@ dependencies {
     // :feature:library:googledrive
     // Type com.google.common.util.concurrent.ListenableFuture is defined multiple times:
     implementation(libs.google.guava)
+
+
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.android.gms:play-services-auth-api-phone:18.0.1")
+    implementation("com.google.android.gms:play-services-auth-base:18.0.10")
+    implementation("com.google.android.gms:play-services-base:18.3.0")
+    implementation("com.google.android.gms:play-services-fido:20.1.0")
 }
